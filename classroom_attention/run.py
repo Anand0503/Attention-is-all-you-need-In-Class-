e74581ddac6fe62cdeae50078253ee4019609b28
Anand <121540934+Anand0503@users.noreply.github.com>
2026-02-17 19:30:21 +0530

    Delete classroom_attention directory

--- classroom_attention/run.py
+++
@@ -1,9 +0,0 @@
-from app.pipeline.processor import ClassroomProcessor
-
-if __name__ == "__main__":
-    processor = ClassroomProcessor()
-
-    processor.process_video(
-        video_path="input.mp4",
-        output_path="outputs/output.avi"
-    )
